b275dce47a1657745e6dfc0be4eda113a30a3df8
takibi6 <momiji9246@gmail.com>
2018-07-09 23:46:15 +0900

    add project

---
+++ TestCrystalProject/Assets/Scripts/CrystalManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalManager : MonoBehaviour {
+
+    public int crystalNum;
+    public float randomSetupPosition;
+    public float searchDistance;
+    public float crystalGetAngle;
+    public float moveSpeed;
+    public float lateSpeed;
+    public float keepDistance;
+    public float disorder;
+
+    public GameObject crystalPrefab;
+    public GameObject targetObject;
+    public GameObject centerMark;
+
+    public GameObject[] crystalObjects;
+    public CrystalMove[] crystalClasses;
+
+	void Start () {
+        crystalObjects = new GameObject[crystalNum];
+        crystalClasses = new CrystalMove[crystalNum];
+        Transform crystalParent = GameObject.Find("Crystals").transform;
+
+        for (int i = 0; i < crystalNum; i++)
+        {
+            crystalObjects[i] = Instantiate(crystalPrefab, crystalParent);
+            crystalObjects[i].transform.position = new Vector3(Random.Range(-randomSetupPosition, randomSetupPosition), Random.Range(-randomSetupPosition, randomSetupPosition), Random.Range(-randomSetupPosition, randomSetupPosition));
+            crystalClasses[i] = crystalObjects[i].GetComponent<CrystalMove>();
+        }
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ TestCrystalProject/Assets/Scripts/PointingController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class PointingController : MonoBehaviour
+{
+    public GameObject ground;
+    public static Vector3 pointerVector;  //マウスポインタのオブジェクトが動いた距離
+
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        Vector3 screenPoint= Input.mousePosition;
+        screenPoint.z = 1;
+
+        Ray ray = Camera.main.ScreenPointToRay(screenPoint);
+        RaycastHit hit;
+        Vector3 targetPosition = Vector3.zero;
+
+        pointerVector = this.transform.position;  //マウスポインタのオブジェクトが動いた距離計算
+
+        if (ground.GetComponent<Collider>().Raycast(ray, out hit, 500))
+        {
+            targetPosition = hit.point;
+            this.gameObject.transform.position =  new Vector3(targetPosition.x, this.transform.position.y, targetPosition.z);
+        }
+        pointerVector -= this.transform.position;  //マウスポインタのオブジェクトが動いた距離計算
+        pointerVector *= -1;
+    }
+}
